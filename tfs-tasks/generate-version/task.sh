2a575bfc97f2cfc707edd518ce19ea03246a94da
David Dieruf <ddieruf@pivotal.io>
2018-03-21 17:23:57 +0000

    Updated task.sh

--- tfs-tasks/generate-version/task.sh
+++ tfs-tasks/generate-version/task.sh
@@ -23,7 +23,8 @@ source "${ARTIFACT_ROOT}/${TASK_SCRIPTS_RESOURCE}/tasks/generate-version/run.sh"
 
 echo "New version number: ${NEW_VERSION_NUMBER}"
 
-
+echo "##vso[task.setvariable variable=VERSION_NUM]${NEW_VERSION_NUMBER}"
+echo "Saved version: ${VERSION_NUM}"
 #######################################
 #       Finalize task
 #######################################
