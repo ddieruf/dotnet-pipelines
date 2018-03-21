be682567f7ebfe4a50a0860750772426339a2845
David Dieruf <ddieruf@pivotal.io>
2018-03-21 14:22:41 +0000

    Updated task.sh

--- tfs-tasks/generate-version/task.sh
+++ tfs-tasks/generate-version/task.sh
@@ -3,9 +3,10 @@
 set -o errexit
 set -o errtrace
 
-ROOT_FOLDER="${AGENT_RELEASEDIRECTORY}"
-TASK_SCRIPTS_RESOURCE="task-scripts"
-SRC_VERSION_RESOURCE="src-version"
+ROOT_FOLDER="${SYSTEM_ARTIFACTSDIRECTORY}"
+PIPELINE_RESOURCE="dotnet-pipelines"
+TASK_SCRIPTS_RESOURCE="${PIPELINE_RESOURCE}/scripts"
+SRC_VERSION_RESOURCE="src-and-test"
 
 #######################################
 #       Initialize Task
@@ -21,8 +22,9 @@ source "${ROOT_FOLDER}/${TASK_SCRIPTS_RESOURCE}/tasks/generate-version/run.sh"
 
 echo "New version number: ${NEW_VERSION_NUMBER}"
 
-#save the new version number to a global variable
-#TODO!!!!
+echo "##vso[task.setvariable variable=MySecret;isSecret=true]My secret value"
+echo "My secret: ${MySecret}"
+exit 1
 
 #######################################
 #       Finalize task
