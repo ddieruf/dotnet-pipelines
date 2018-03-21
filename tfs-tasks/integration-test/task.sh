bf60ab75295f6d91060508c03bdaff94c6aef15a
David Dieruf <ddieruf@pivotal.io>
2018-03-21 17:03:20 +0000

    Updated task.sh

--- tfs-tasks/integration-test/task.sh
+++ tfs-tasks/integration-test/task.sh
@@ -3,8 +3,10 @@
 set -o errexit
 set -o errtrace
 
-ROOT_FOLDER="${AGENT_RELEASEDIRECTORY}"
-TASK_SCRIPTS_RESOURCE="task-scripts"
+ROOT_FOLDER="${AGENT_WORKFOLDER}"
+ARTIFACT_ROOT="${SYSTEM_ARTIFACTSDIRECTORY}"
+PIPELINE_RESOURCE="dotnet-pipelines"
+TASK_SCRIPTS_RESOURCE="${PIPELINE_RESOURCE}/scripts"
 
 #######################################
 #       Initialize Task
@@ -13,10 +15,13 @@ TASK_SCRIPTS_RESOURCE="task-scripts"
 #######################################
 #       Run Task
 #######################################
+#ARTIFACT_LOCATION_TYPE
 #ARTIFACTORY_HOST
 #ARTIFACTORY_TOKEN
 #ARTIFACTORY_REPO_ID
+#ARTIFACT_FOLDER_PATH
 #DOTNET_VERSION
+
 export TEST_DLL_NAME="${INTEGRATION_TEST_DLL_NAME}"
 export TEST_ARTIFACT_NAME="${INTEGRATION_TEST_ARTIFACT_NAME}"
 source "${ROOT_FOLDER}/${TASK_SCRIPTS_RESOURCE}/tasks/dotnet-test/run.sh"
