b7cd3bfd2f5e208f9abdaa62bc582c0cd994301c
David Dieruf <ddieruf@pivotal.io>
2018-03-23 01:40:41 +0000

    Updated task.sh

--- tfs-tasks/generate-version/task.sh
+++ tfs-tasks/generate-version/task.sh
@@ -9,6 +9,7 @@ PIPELINE_RESOURCE="dotnet-pipelines/drop"
 TASK_SCRIPTS_RESOURCE="${PIPELINE_RESOURCE}/scripts"
 SRC_VERSION_RESOURCE="src-and-test"
 
+
 #######################################
 #       Initialize Task
 #######################################
@@ -17,6 +18,7 @@ SRC_VERSION_RESOURCE="src-and-test"
 #       Run Task
 #######################################
 export VERSION_ROOT="${ARTIFACT_ROOT}/${SRC_VERSION_RESOURCE}"
+export ARTIFACT_FOLDER_PATH=${BUILD_ARTIFACTSTAGINGDIRECTORY}
 #GIT_EMAIL
 #GIT_NAME
 source "${ROOT_FOLDER}/${TASK_SCRIPTS_RESOURCE}/tasks/generate-version/run.sh"
@@ -25,10 +27,8 @@ echo "New version number: ${NEW_VERSION_NUMBER}"
 
 echo "##vso[task.setvariable variable=MY_OUTPUT_VAR]${NEW_VERSION_NUMBER}"
 
-echo "Folder path: ${!ARTIFACT_FOLDER_PATH}"
-
-env
-
+echo "Folder path: ${BUILD_ARTIFACTSTAGINGDIRECTORY}"
+echo "Folder path: ${ARTIFACT_FOLDER_PATH}"
 
 #######################################
 #       Finalize task
