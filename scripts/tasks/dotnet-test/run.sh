2abc2068e5a11a34428098510c7ca888db67262b
David Dieruf <david@dierufventures.com>
2018-03-24 10:43:40 -0400

    fix

--- scripts/tasks/dotnet-test/run.sh
+++ scripts/tasks/dotnet-test/run.sh
@@ -35,7 +35,7 @@ export TEST_EXTRACT="test-extract"
 case "${ARTIFACT_LOCATION_TYPE}" in
   "local")
     [[ ! -z "${ARTIFACT_FOLDER_PATH}" ]] || (echo "ARTIFACT_FOLDER_PATH is a required value" && exit 1)
-    [[ -f "${ARTIFACT_FOLDER_PATH}" ]] || (echo "ARTIFACT_FOLDER_PATH path invalid [${ARTIFACT_FOLDER_PATH}]" && exit 1)
+    [[ -d "${ARTIFACT_FOLDER_PATH}" ]] || (echo "ARTIFACT_FOLDER_PATH path invalid [${ARTIFACT_FOLDER_PATH}]" && exit 1)
     ;;
   "artifactory")
     [[ ! -z "${ARTIFACTORY_HOST}" ]] || (echo "ARTIFACTORY_HOST is a required value" && exit 1)
