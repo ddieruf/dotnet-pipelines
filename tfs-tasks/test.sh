709cab8d50545739896c0e93a9466830bbc1760e
David Dieruf <david@dierufventures.com>
2018-03-26 08:08:25 -0400

    ff

---
+++ tfs-tasks/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o errtrace
+
+ROOT_FOLDER="${SYSTEM_DEFAULTWORKINGDIRECTORY}"
+ARTIFACT_ROOT="${SYSTEM_ARTIFACTSDIRECTORY}"
+PIPELINE_RESOURCE="dotnet-pipelines"
+TASK_SCRIPTS_RESOURCE="${PIPELINE_RESOURCE}/scripts"
+SRC_AND_TEST_RESOURCE="src-and-test"
+
+
+git clone "${ARTIFACT_ROOT}/${SRC_AND_TEST_RESOURCE}" "${ARTIFACT_ROOT}/updated-version"
+pushd "${ARTIFACT_ROOT}/updated-version"
+  git config --local user.email "my@emai.com"
+  git config --local user.name "My Name"
+
+  touch version
+  echo "123asd" > version
+
+  git add version
+  git commit -m "Commit this file"
+popd
+
+ls -l "${ARTIFACT_ROOT}/updated-version"
