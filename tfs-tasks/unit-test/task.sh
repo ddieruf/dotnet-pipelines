d1c7a80f40009a6d7b24f6b3a4df42e81a91cfb3
David Dieruf <david@dierufventures.com>
2018-03-26 15:04:20 -0400

    aa

--- tfs-tasks/unit-test/task.sh
+++ tfs-tasks/unit-test/task.sh
@@ -7,27 +7,25 @@ ROOT_FOLDER="${SYSTEM_DEFAULTWORKINGDIRECTORY}"
 ARTIFACT_ROOT="${SYSTEM_ARTIFACTSDIRECTORY}"
 PIPELINE_RESOURCE="dotnet-pipelines"
 TASK_SCRIPTS_RESOURCE="${PIPELINE_RESOURCE}/scripts"
-SRC_AND_TEST_RESOURCE="src-and-test"
+SRC_AND_TEST_RESOURCE="src-and-test/drop"
 
 #######################################
 #       Initialize Task
 #######################################
-while IFS='=' read -r name value ; do
-    if [[ "${name}" == *'UNIT_TEST_ARTIFACT_NAME' ]]; then
-       export TEST_ARTIFACT_NAME="${value}"
-    fi
-done < <(env)
 
 #######################################
 #       Run Task
 #######################################
-#ARTFACT_LOCATION_TYPE
+#ARTIFACT_LOCATION_TYPE
 #ARTIFACTORY_HOST
 #ARTIFACTORY_TOKEN
 #ARTIFACTORY_REPO_ID
 #DOTNET_VERSION
+
 export TEST_DLL_NAME="${UNIT_TEST_DLL_NAME}"
-export ARTIFACT_FOLDER_PATH="${ARTIFACT_ROOT}"
+export ARTIFACT_FOLDER_PATH="${ARTIFACT_ROOT}/${SRC_AND_TEST_RESOURCE}"
+export TEST_ARTIFACT_NAME=$(find ./${ARTIFACT_FOLDER_PATH} -name '*UnitTest*')
+echo ${TEST_ARTIFACT_NAME}
 
 source "${ARTIFACT_ROOT}/${TASK_SCRIPTS_RESOURCE}/tasks/dotnet-test/run.sh"
 
